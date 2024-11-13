b017b9e51c181e38c42b9ce6745cfd2c2272b6fa
Parker Reno Ciambrone <parkerreno@msn.com>
2024-11-13 12:16:39 -0800

    messy stuff - wip

--- Meshtastic/Connections/BleConnection.cs
+++ Meshtastic/Connections/BleConnection.cs
@@ -2,39 +2,109 @@ using Google.Protobuf;
 using Meshtastic.Data;
 using Meshtastic.Protobufs;
 using Microsoft.Extensions.Logging;
+using Plugin.BLE.Abstractions.Contracts;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Meshtastic.Connections;
 
+/// <summary>
+/// 
+/// </summary>
+[Experimental(diagnosticId: "BLESupport")]
 public class BleConnection : DeviceConnection, IDisposable
 {
-    public BleConnection(ILogger logger) : base(logger)
+    private readonly IDevice bleDevice;
+    private readonly IAdapter? bleAdapter;
+    
+    private IService? meshtasticService;
+    private ICharacteristic toRadio;
+    private ICharacteristic fromRadio;
+    private ICharacteristic fromNum;
+    private ICharacteristic logRadio;
+    private ICharacteristic legacyLogRadio;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="device"></param>
+    /// <param name="logger"></param>
+    /// <param name="adapter"></param>
+    public BleConnection(IDevice device, ILogger logger, IAdapter? adapter = null) : base(logger)
     {
+        this.bleDevice = device;
+        this.bleAdapter = adapter;
+
+        logger.LogInformation($"BleConnection started to device {device.Name}. Connection state: {device.State}. Pairing state: {device.BondState}.");
+        if (adapter is null)
+        {
+            logger.LogInformation("Bluetooth adapter was not provided - not all functionality will be available.");
+        }
     }
 
-    public BleConnection(ILogger logger, DeviceStateContainer container) : base(logger, container)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="device"></param>
+    /// <param name="container"></param>
+    /// <param name="logger"></param>
+    /// <param name="adapter"></param>
+    public BleConnection(IDevice device, DeviceStateContainer container, ILogger logger, IAdapter? adapter = null) : this (device, logger, adapter)
     {
+        this.DeviceStateContainer = container;
     }
 
-    public override void Disconnect()
+    /// <inheritdoc/>
+    /// <exception cref="InvalidOperationException">This will be thrown if an adapter was not passed into the constructor on instantiation.</exception>
+    public override async void Disconnect()
     {
-        throw new NotImplementedException();
+        if (bleAdapter is null)
+        {
+            throw new InvalidOperationException("No Bluetooth adapter was provided - adapter must be provided on instantiation or method overload must be used.");
+        }
+
+        await this.Disconnect(bleAdapter);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="adapter">BLE Adapter that is connected to the device</param>
+    /// <returns>Task completes when device is disconnected</returns>
+    public async Task Disconnect(IAdapter adapter)
+    {
+        await adapter.DisconnectDeviceAsync(bleDevice);
+    }
+
+    /// <inheritdoc/>
     public override Task ReadFromRadio(Func<FromRadio?, DeviceStateContainer, Task<bool>> isComplete, int readTimeoutMs = 15000)
     {
+        isComplete(null, this.DeviceStateContainer);
         throw new NotImplementedException();
     }
 
+    /// <inheritdoc/>
     public override Task<DeviceStateContainer> WriteToRadio(ToRadio toRadio, Func<FromRadio, DeviceStateContainer, Task<bool>> isComplete)
     {
         throw new NotImplementedException();
     }
 
+    /// <inheritdoc/>
     public override Task WriteToRadio(ToRadio toRadio)
     {
         throw new NotImplementedException();
     }
 
+    private async Task ConnectToService()
+    {
+        if (meshtasticService is null)
+        {
+            meshtasticService = await bleDevice.GetServiceAsync(Resources.SERVICE_GUID);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
     public void Dispose()
     {
         throw new NotImplementedException();
--- Meshtastic/Resources.cs
+++ Meshtastic/Resources.cs
@@ -6,4 +6,12 @@ public static class Resources
 
     public const int DEFAULT_READ_TIMEOUT = 15000;
     public const int MAX_TO_FROM_RADIO_LENGTH = 512;
+
+    /* Bluetooth Service GUID Constants */
+    public static readonly Guid SERVICE_GUID = Guid.Parse("6ba1b218-15a8-461f-9fa8-5dcae273eafd");
+    public static readonly Guid TO_RADIO_GUID = Guid.Parse("f75c76d2-129e-4dad-a1dd-7866124401e7");
+    public static readonly Guid FROM_RADIO_GUID = Guid.Parse("2c55e69e-4993-11ed-b878-0242ac120002");
+    public static readonly Guid FROM_NUM_GUID = Guid.Parse("ed9da18c-a800-4f66-a670-aa7547e34453");
+    public static readonly Guid LOG_RADIO_GUID = Guid.Parse("5a3d6e49-06e6-4423-9944-e9de8cdf9547");
+    public static readonly Guid LEGACY_LOG_RADIO_GUID = Guid.Parse("6c6fd238-78fa-436b-aacf-15c5be1ef2e2");
 }
